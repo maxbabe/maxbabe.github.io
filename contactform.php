b99b958219953cb1e3dbe8583d791dcab85edb1e
maxbabe <48551612+maxbabe@users.noreply.github.com>
2019-03-15 12:32:17 +0200

    Add files via upload

---
+++ contactform.php
@@ -0,0 +1,18 @@
+<?php>
+
+if (issett($_POST['submit'])) {
+  
+  $name = $_POST["name"];
+  $mailFrom = $_POST['email'];
+  $message = $_POST['message'];
+  
+  $mailTo = "tracy@sa-ems.co.za"
+  $headers = "From: ".$mailFrom;
+  $txt = "You have received email from ". $name;  ".n/n/"   .$message;
+
+  
+  mail($mailTo ,$headers, $txt);
+  header("Location: thankyou.html");
+    # code...
+}
+
